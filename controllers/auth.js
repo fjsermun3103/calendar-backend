941fef0e5fd3f299337a5973809ede7655181847
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-06-24 12:21:45 +0200

    Commit inicial

---
+++ controllers/auth.js
@@ -0,0 +1,58 @@
+const {response} = require('express');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async(req, res = response ) => {
+    const { email, password } = req.body;
+    try {
+        let usuario = await Usuario.findOne({ email });
+
+        if( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo',
+            });
+        }
+
+        usuario = new Usuario( req.body );
+    
+        await usuario.save();
+        
+        return res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+}
+
+const loginUsuario = (req, res = response) => {
+    const {email, password} = req.body;
+
+    return res.status(201).json({
+        ok: true,
+        msg: 'login',
+        email,
+        password,
+        
+    })
+}
+
+const revalidarToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
+    })
+};
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
+}
+
